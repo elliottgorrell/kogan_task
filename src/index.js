b83fc353e889f2199d81545cf81fd92415ce5ed9
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-05-12 10:04:42 +1000

    We now can keep querying util we have all products

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const axios = require('axios');
 
 const endpoint = axios.create({
-  baseURL: 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products',
+  baseURL: 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com',
   timeout: 3000,
 });
 
-const getDataFromEndpoint = async () => {
-  const response = await endpoint.get('/1');
+const getDataFromEndpoint = async (route) => {
+  const response = await endpoint.get(route);
   return response.data;
 };
 
-getDataFromEndpoint()
+const getAllData = async () => {
+  let response = await getDataFromEndpoint('/api/products/1');
+  let data = response.objects;
+  while (response.next) {
+    response = await getDataFromEndpoint(response.next);
+    data = data.concat(response.objects);
+  }
+  return data;
+};
+
+
+getAllData()
   .then(data => console.log(data))
   .catch(e => console.error('Error:', e));
